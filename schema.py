ccc072f515d311a9c53c7eacd8d4adcff7be7dd8
Oyinda David <oyindadavid18@gmail.com>
2021-02-04 16:22:51 +0100

    params to schema execute must be in camel case

--- schema.py
+++ schema.py
@@ -3,20 +3,27 @@ import json
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
+    
+    def resolve_is_admin(self, info):
+        return True
 
 
 schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     '''
-    {
+    query {
         hello
+        isAdmin
     }
     '''
 )
 
 dictResult = dict(result.data.items())
 print(json.dumps(dictResult, indent=2))
+
+# print(result)
